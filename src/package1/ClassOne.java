6d0560f5a9b05842f6285e5b9bb7753231b8b4db
Ibrahim Kalin <hisetkal@gmail.com>
2021-08-11 18:51:52 -0400

    4th commit

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -5,5 +5,6 @@ public class ClassOne {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Welcome Bank project members!");
+		System.out.println("here is a fouth commit");
 	}
 }
